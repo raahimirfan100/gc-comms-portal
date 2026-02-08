240a8538728f3c811214d40d13bed8c1db0832da
Hammaad Salman <hammaad@umich.edu>
2026-02-09 03:48:54 +0500

    Initial commit from Create Next App

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  cacheComponents: true,
+};
+const nextConfig: NextConfig = {
+  cacheComponents: true,
+};
 
 export default nextConfig;
